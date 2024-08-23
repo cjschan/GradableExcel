91425dceb9bd4fbc6b5db841194b4c56301332ba
cjschan <cjschan@gmail.com>
2024-08-23 15:37:13 -0500

    Update taskpane.js

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -36,25 +36,54 @@ export async function writeData() {
   }
 }
 
-async function check() {
-  await Excel.run(async (context) => {
-    let ws = context.workbook.worksheets.getActiveWorksheet();
-    let answer = ws.getRange("S6");
-    let fmla = `=IF(ISFORMULA(D2),IF(FORMULATEXT(D2)<>"=CORREL(A2:A6,B2:B6)",
-"Incorrect. The formula in D2 has an error.","Correct. The formula typed is:  =CORREL(A2:A6,B2:B6)"),
-"Incorrect. Please type a formula in cell D2.")`;
-    answer.values = [[fmla]];
-    let response = ws.getRange("D4");
-    response.values = [["=S6"]];
-    await context.sync();
-  });
-}
+// async function check() {
+//   await Excel.run(async (context) => {
+//     let ws = context.workbook.worksheets.getActiveWorksheet();
+//     let answer = ws.getRange("S6");
+//     let fmla = `=IF(ISFORMULA(D2),IF(FORMULATEXT(D2)<>"=CORREL(A2:A6,B2:B6)",
+// "Incorrect. The formula in D2 has an error.","Correct. The formula typed is:  =CORREL(A2:A6,B2:B6)"),
+// "Incorrect. Please type a formula in cell D2.")`;
+//     answer.values = [[fmla]];
+//     let response = ws.getRange("D4");
+//     response.values = [["=S6"]];
+//     await context.sync();
+//   });
+// }
 
 async function reset() {
   await Excel.run(async (context) => {
     let ws = context.workbook.worksheets.getActiveWorksheet();
     let range = ws.getRange("D2:S6");
     range.clear();
+    await context.sync();
+  });
+}
+
+async function check() {
+  // document.getElementById("feedback").textContent = "Hello";
+  await Excel.run(async (context) => {
+    let ws = context.workbook.worksheets.getActiveWorksheet();
+    let response = ws.getRange("D2");
+    // response.values = [["=S6"]];
+    let data = ws.getRange("A2");
+    response.load("formulas");
+    data.load("values");
+    await context.sync();
+    let form_str = response.formulas[0];
+    let text1 = "";
+    if (data.values[0][0] == "") {
+      text1 = text1 + `Select "Generate Data"`;
+    } else if (form_str == "") {
+      text1 = text1 + `Type a formula in cell D2.  Excel formulas begin with "=".`;
+    } else if (form_str == "=CORREL(A2:A6,B2:B6)") {
+      text1 = text1 + "Correct. The formula typed is:  =CORREL(A2:A6,B2:B6)";
+    } else if (JSON.stringify(form_str).includes("=")) {
+      text1 = text1 + "Incorrect. The formula in D2 has an error.";
+    } else {
+      text1 = text1 + `Incorrect. Type a formula in cell D2. Excel formulas begin with "=".`;
+    }
+    let feedback = ws.getRange("D4");
+    feedback.values = text1;
     await context.sync();
   });
 }
